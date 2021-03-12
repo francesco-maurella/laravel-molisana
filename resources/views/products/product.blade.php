7fc494d4ead6eae204a59da88f5fd8a170a1f302
Francesco Maurella <francesco.maurella@live.it>
2021-03-12 17:51:14 +0100

    Stili prodotto

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -1,11 +1,17 @@
 @extends('layout')
 @section('main')
-  <main>
-    <ul>
-      <li>
-        <img src="{{$pastaList[$idProduct]['src']}}" alt="{{$pastaList[$idProduct]['titolo']}}">
-        <div>{{$pastaList[$idProduct]['titolo']}}</div>
-      </li>
-    </ul>
+  <main class="product-content">
+    <div class="container">
+      <div class="product-content-top">
+        <div>
+          <img src="{{$pastaList[$idProduct]['src-h']}}" alt="{{$pastaList[$idProduct]['titolo']}}">
+        </div>
+        <div>
+          <img src="{{$pastaList[$idProduct]['src-p']}}" alt="{{$pastaList[$idProduct]['titolo']}}">
+        </div>
+        <div class="title">{{$pastaList[$idProduct]['titolo']}}</div>
+      </div>
+      <div class="product-content-bottom">{!!$pastaList[$idProduct]['descrizione']!!}</div>
+      </div>
   </main>
 @endsection
